065c490e55a6f1c6dea806bc97ae0f0c0d62559e
danwheeler <da-wheeler@hotmail.co.uk>
2015-10-20 12:40:03 +0100

    Chapter 10 & 11 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,29 +1,31 @@
 class Oystercard
-  attr_reader :balance
+
+   attr_reader :balance, :entry_station
 
   TOP_UP_LIMIT = 90
   MIN_FARE = 1
 
-  def initialize
-    @balance = 0
-    @in_journey = false
+  def initialize (balance = 0)
+    @balance = balance
+    @entry_station = nil
   end
 
   def top_up amount
     ((@balance + amount) > TOP_UP_LIMIT) ? (raise "Top up limit #{TOP_UP_LIMIT} exceeded") : (@balance += amount)
   end
 
-  def touch_in
+  def touch_in(station = :station)
   	(@balance < 1) ? (raise "Insufficient funds" ): (@in_journey = true)
+    @entry_station = station
   end
 
   def touch_out
     deduct
-    @in_journey = false
+    @entry_station = nil
   end
 
   def in_journey?
-    @in_journey
+    @entry_station != nil
   end
 
   private
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,36 +1,59 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'test balance' do
-    expect(subject.balance).to eq(0)
+  let(:waterloo) {double :waterloo}
+  subject(:oystercard) {described_class.new}
+  subject(:oystercard20) {described_class.new(20)}
+
+  context "On Initialise it" do
+    it 'tests starting balance is zero' do
+      expect(oystercard.balance).to eq(0)
+    end
   end
 
-  it "top's up oystercard balance" do
-  	expect(subject.top_up(10)).to eq (10)
+  context "Topping up it" do
+    it "top's up oystercard balance" do
+    	expect(oystercard.top_up(10)).to eq (10)
+    end
+    it 'raises error if top up exceeds limit' do
+      expect{oystercard.top_up(Oystercard::TOP_UP_LIMIT+1)}.to raise_error "Top up limit #{Oystercard::TOP_UP_LIMIT} exceeded"
+    end
   end
 
-  it 'raises error if top up exceeds limit' do
-    expect{subject.top_up(Oystercard::TOP_UP_LIMIT+1)}.to raise_error "Top up limit #{Oystercard::TOP_UP_LIMIT} exceeded"
+  context "Touching in" do
+    it 'changes status of journey to travelling' do
+    	oystercard.top_up(Oystercard::MIN_FARE)
+      oystercard.touch_in
+      expect(oystercard.in_journey?).to eq true
+    end
+    it 'raises an error when balance is smaller than minimum fair' do
+    	expect{oystercard.touch_in}.to raise_error "Insufficient funds"
+    end
+    it 'records the touch in station' do
+      oystercard20.touch_in(waterloo)
+      expect(oystercard20.entry_station).to eq waterloo
+    end
   end
 
-  it 'touch_in' do
-  	subject.top_up(Oystercard::MIN_FARE)
-    expect(subject.touch_in).to eq true
+  context "Touching out" do
+    it 'changes status of journey to not travelling' do
+      oystercard20.touch_in
+      oystercard20.touch_out
+      expect(oystercard20.in_journey?).to eq false
+    end
+    it 'touch_out reduces balance by MIN_FARE' do
+      expect{oystercard20.touch_out}.to change{subject.balance}.by(-Oystercard::MIN_FARE)
+    end
+    it 'changes the entry station to nil on touch out' do
+      oystercard20.touch_in(waterloo)
+      oystercard20.touch_out
+      expect(oystercard20.entry_station).to eq nil
+    end
   end
-
-  it 'raises an error when balance is smaller than minimum fair' do
-  	expect{subject.touch_in}.to raise_error "Insufficient funds"
-  end
-
-  it 'touch_out' do
-    expect(subject.touch_out).to eq false
-  end
-
-  it 'touch_out reduces balance by MIN_FARE' do
-    expect{subject.touch_out}.to change{subject.balance}.by(-Oystercard::MIN_FARE)
-  end
-
-  it 'in_journey?' do
-    expect(subject.in_journey?).to eq false
+  context "In Journey" do
+    it 'tells user if they are currently travelling' do
+      oystercard20.touch_in
+      expect(oystercard20.in_journey?).to eq true
+    end
   end
 end
